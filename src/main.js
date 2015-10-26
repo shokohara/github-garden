8b9f9a236f0883f7b2fcd302847eab9ecde033c1
skohar <skohar@users.noreply.github.com>
2015-10-26 10:52:18 +0900

    disable main window

--- src/main.js
+++ src/main.js
@@ -38,24 +38,24 @@ mb.on("ready", () => {
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
-app.on("ready", () => {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-
-    // and load the index.html of the app.
-    mainWindow.loadUrl(`file://${__dirname}/index.html`);
-    // mainWindow.loadUrl(`https://github.com/skohar`);
-
-    // Open the devtools.
-    if (process.env.NODE_ENV !== "production") {
-        mainWindow.openDevTools();
-    }
-
-    // Emitted when the window is closed.
-    mainWindow.on("closed", () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
-});
+// app.on("ready", () => {
+//     // Create the browser window.
+//     mainWindow = new BrowserWindow({width: 800, height: 600});
+//
+//     // and load the index.html of the app.
+//     mainWindow.loadUrl(`file://${__dirname}/index.html`);
+//     // mainWindow.loadUrl(`https://github.com/skohar`);
+//
+//     // Open the devtools.
+//     if (process.env.NODE_ENV !== "production") {
+//         mainWindow.openDevTools();
+//     }
+//
+//     // Emitted when the window is closed.
+//     mainWindow.on("closed", () => {
+//         // Dereference the window object, usually you would store windows
+//         // in an array if your app supports multi windows, this is the time
+//         // when you should delete the corresponding element.
+//         mainWindow = null;
+//     });
+// });
