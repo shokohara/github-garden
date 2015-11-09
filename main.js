8f3213ace202cdba0496706306af09a524edb45a
shokohara <shokohara@users.noreply.github.com>
2015-11-09 17:19:07 +0900

    Show last updated time

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = require("app");
 var BrowserWindow = require("browser-window");
 var Tray = require("tray");
 var Menu = require("menu");
+var moment = require("moment");
 require("crash-reporter").start();
 
 var mainWindow = null;
@@ -31,30 +32,46 @@ app.on("ready", function() {
   mainWindow.hide();
 
   tray = new Tray(__dirname + "/images/#eeeeee.png");
-  tray.setContextMenu(Menu.buildFromTemplate([
-    {
-      label: "Preference",
-      click: function () {
-        if (mainWindow == null) {
-          createWindow()
-        } else {
-          mainWindow.show();
+  function createContextMenu(newLabel) {
+    return Menu.buildFromTemplate([
+      {
+        label: newLabel,
+        enabled: false
+      },
+      {
+        label: "GitHub Garden v" + app.getVersion(),
+        enabled: false
+      },
+      {
+        label: "Preference",
+        click: function () {
+          if (mainWindow == null) {
+            createWindow()
+          } else {
+            mainWindow.show();
+          }
+        }
+      },
+      {
+        label: "Quit",
+        click: function () {
+          app.quit();
         }
       }
-    },
-    {
-      label: "Quit",
-      click: function () {
-        app.quit();
-      }
-    }
-  ]));
+    ]);
+  }
+  tray.setContextMenu(createContextMenu("Fetching"));
 
   ipc.on("asynchronous-message", function (event, arg) {
     var color = arg === null ? "#eeeeee" : arg;
     var src = __dirname + "/images/" + color + ".png";
     tray.setImage(src);
     tray.setPressedImage(src);
+    var lastUpdatedAt = new Date().getTime();
+    var newLabel = "Last updated at " + moment(lastUpdatedAt).format('HH:mm:ss');
+    if (tray.menu.items[0].label !== newLabel) {
+      tray.setContextMenu(createContextMenu(newLabel))
+    }
   });
   ipc.on("asynchronous-message2", function (event, arg) {
     if (mainWindow == null) {
