40f77589b92c3e20acddf88c0ac8f3540d0431df
skohar <skohar@users.noreply.github.com>
2016-02-22 15:52:41 +0900

    Test deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 $(npm bin)/electron-packager . github-garden --out dist --platform=darwin --arch=x64 --version=0.34.3
+if [[ $(git tag) == '' ]]; then
+  git tag 'release-v0.0.0'
+fi
 oldVersion=$(git describe --tags | sed -e 's/release-//g' | sed -e 's/v//g')
 oldBuildVersion=$(echo $oldVersion | sed -e 's/^[[:digit:]].[[:digit:]].//g')
 currentBuildVersion=$(echo "$oldBuildVersion+1" | bc)
